8976d319056944c99dab873815055d6957763bce
Jonathan Giddy <jongiddy@gmail.com>
2021-07-04 11:28:31 +0100

    Use lol-html on_end_tag for extraction

--- src/extract.rs
+++ src/extract.rs
@@ -2,7 +2,8 @@ use lol_html::errors::RewritingError;
 use lol_html::html_content::UserData;
 use lol_html::{element, text, HtmlRewriter, Settings};
 use serde::Serialize;
-use std::cell::{Cell, RefCell};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Debug, Serialize)]
 pub(crate) struct Row {
@@ -16,161 +17,215 @@ pub(crate) struct Row {
 }
 
 pub(crate) fn extract_rows(series: u16, page: String) -> Result<Vec<Row>, RewritingError> {
-    let output: RefCell<Vec<Row>> = RefCell::new(vec![]);
+    let output = Rc::new(RefCell::<Vec<Row>>::new(vec![]));
 
-    #[derive(PartialEq)]
+    #[derive(Debug, PartialEq)]
     enum State {
-        ExpectNone,
         ExpectRow,
-        ExpectCouple(i32),
-        SkipCouple(i32, String),
-        ExpectScore(i32, String),
-        ExpectDance(i32, String, u8),
-        ExpectEnd(i32, String),
+        ExpectCouple,
+        ExpectScore,
+        ExpectDance,
+        ExpectEnd,
     }
+    #[derive(Debug)]
     struct Shared {
         state: State,
         week: u16,
+        couple: String,
+        couple_uses: u8,
+        score: String,
+        score_uses: u8,
+        dance: String,
+        dance_uses: u8,
+        note: String,
     }
     impl Default for Shared {
         fn default() -> Self {
             Shared {
-                state: State::ExpectNone,
+                state: State::ExpectRow,
                 week: 0,
+                couple: String::new(),
+                couple_uses: 0,
+                score: String::new(),
+                score_uses: 0,
+                dance: String::new(),
+                dance_uses: 0,
+                note: String::new(),
             }
         }
     }
     // Cell mutability for shared and mutable access from multiple closures.
-    let buffer = RefCell::new(String::new());
-    let shared = Cell::new(Shared::default());
+    let shared = Rc::new(RefCell::new(Shared::default()));
     let mut rewriter = HtmlRewriter::new(
         Settings {
             element_content_handlers: vec![
                 // Find week number
                 element!("span.mw-headline", |el| {
-                    shared.set(Shared::default());
+                    let mut s = Shared::default();
                     if let Some(id) = el.get_attribute("id") {
                         // "Week_1", "Week_6:_Quarter-final"
                         let mut parts = id.split(&['_', ':'][..]);
                         if let Some("Week") = parts.next() {
-                            let week = parts
+                            s.week = parts
                                 .next()
                                 .ok_or_else(|| format!("Bad parse {}", id))?
                                 .parse()?;
-                            if series == 10 && week == 10 {
-                                // Unsupported table format - handle specially. In this week,
-                                // couples combined two styles in one dance.  For analysis
-                                // treat them as separate dances with the same score.
-                                let table = [
-                                    ("Denise", "James", "Jive", "Quickstep", 35),
-                                    ("Lisa", "Robin", "Cha-Cha-Cha", "Tango", 30),
-                                    ("Nicky", "Karen", "American Smooth", "Samba", 27),
-                                    ("Dani", "Vincent", "Charleston", "Quickstep", 38),
-                                    ("Louis", "Flavia", "Tango", "Rumba", 37),
-                                    ("Kimberley", "Pasha", "Cha-Cha-Cha", "Tango", 40),
-                                ];
-                                let mut vector = output.borrow_mut();
-                                for row in table {
-                                    let (celebrity, professional, dance1, dance2, score) = row;
-                                    let note = format!(
-                                        "Series 10 combined dance: {} & {}",
-                                        dance1, dance2
-                                    );
-                                    vector.push(Row {
-                                        series,
-                                        week,
-                                        celebrity: celebrity.to_owned(),
-                                        professional: professional.to_owned(),
-                                        dance: dance1.to_owned(),
-                                        score,
-                                        note: note.clone(),
-                                    });
-                                    vector.push(Row {
-                                        series,
-                                        week,
-                                        celebrity: celebrity.to_owned(),
-                                        professional: professional.to_owned(),
-                                        dance: dance2.to_owned(),
-                                        score,
-                                        note,
-                                    });
-                                }
-                                return Ok(());
-                            }
-                            shared.set(Shared {
-                                state: State::ExpectRow,
-                                week,
-                            });
                         }
                     }
+                    shared.replace(s);
                     Ok(())
                 }),
-                element!("td", |el| {
-                    let text = buffer.replace(String::new());
-                    let mut s = shared.take();
+                element!("tr", |tr| {
+                    let mut s = (*shared).borrow_mut();
                     if s.week == 0 {
                         return Ok(());
                     }
                     s.state = match s.state {
                         State::ExpectRow => {
-                            // When couples dance multiple dances in a show, the Couples column will
-                            // have a rowspan > 1. Pass the rowspan through the states and reuse the
-                            // couple for the next row.
-                            let rows = match el.get_attribute("rowspan") {
-                                Some(rowspan) => rowspan.parse()?,
-                                None => 1,
-                            };
-                            State::ExpectCouple(rows)
+                            if s.couple_uses == 0 {
+                                State::ExpectCouple
+                            } else if s.score_uses == 0 {
+                                State::ExpectScore
+                            } else if s.dance_uses == 0 {
+                                State::ExpectDance
+                            } else {
+                                State::ExpectEnd
+                            }
                         }
-                        State::ExpectCouple(rows) => {
-                            let couple = html_escape::decode_html_entities(&text).trim().to_owned();
-                            State::ExpectScore(rows, couple)
+                        _ => {
+                            panic!("Unexpected state {:?}", s);
                         }
-                        State::SkipCouple(rows, couple) => State::ExpectScore(rows, couple),
-                        State::ExpectScore(rows, couple) => {
-                            // "27 (7,7,8,5)\n"
-                            match html_escape::decode_html_entities(&text)
-                                .trim()
-                                .split_whitespace()
-                                .next()
-                                .unwrap_or("N/A")
-                                .parse()
-                            {
-                                Ok(score) => State::ExpectDance(rows, couple, score),
-                                Err(_error) => {
-                                    // e.g. "N/A\n" for unscored show dance
-                                    // ignore this row
-                                    State::ExpectEnd(rows - 1, couple)
+                    };
+                    let output = output.clone();
+                    let shared = shared.clone();
+                    tr.on_end_tag(move |_| {
+                        let mut s = (*shared).borrow_mut();
+                        if s.state != State::ExpectEnd {
+                            // This should only occur for the header row that contains no
+                            // td elements and where there is no couple set.
+                            assert!(s.state == State::ExpectCouple, "{:?}", s);
+                            s.state = State::ExpectRow;
+                            return Ok(());
+                        }
+                        assert!(s.state == State::ExpectEnd, "series={} {:?}", series, s);
+                        assert!(s.couple.len() > 0);
+                        assert!(s.couple_uses > 0);
+                        assert!(s.score.len() > 0);
+                        assert!(s.score_uses > 0);
+                        assert!(s.dance.len() > 0);
+                        assert!(s.dance_uses > 0);
+                        let dance = html_escape::decode_html_entities(&s.dance)
+                            .trim()
+                            .to_owned();
+                        let couple = html_escape::decode_html_entities(&s.couple)
+                            .trim()
+                            .to_owned();
+                        let mut i = couple.split(" & ");
+                        let celebrity = i.next().unwrap().to_owned();
+                        let professional = i.next().unwrap().to_owned();
+                        match i.next() {
+                            Some(_) => {
+                                // Dance with multiple couples (e.g. Series 7 week 11)
+                                // ignore
+                            }
+                            None => {
+                                match html_escape::decode_html_entities(&s.score)
+                                    .trim()
+                                    .split_whitespace()
+                                    .next()
+                                    .unwrap_or("N/A")
+                                    .parse()
+                                {
+                                    Ok(score) => {
+                                        (*output).borrow_mut().push(Row {
+                                            series,
+                                            week: s.week,
+                                            celebrity,
+                                            professional,
+                                            dance,
+                                            score,
+                                            note: s.note.clone(),
+                                        });
+                                    }
+                                    Err(_error) => {
+                                        // e.g. "N/A\n" for unscored show dance
+                                        // ignore this row
+                                    }
                                 }
                             }
                         }
-                        State::ExpectDance(rows, couple, score) => {
-                            let dance = html_escape::decode_html_entities(&text).trim().to_owned();
-                            let mut i = couple.split(" & ");
-                            let celebrity = i.next().unwrap().to_owned();
-                            let professional = i.next().unwrap().to_owned();
-                            assert!(i.next().is_none());
-                            let row = Row {
-                                series,
-                                week: s.week,
-                                celebrity,
-                                professional,
-                                dance,
-                                score,
-                                note: String::new(),
-                            };
-                            output.borrow_mut().push(row);
-                            State::ExpectEnd(rows - 1, couple)
+                        s.couple_uses -= 1;
+                        s.score_uses -= 1;
+                        s.dance_uses -= 1;
+                        s.state = State::ExpectRow;
+                        Ok(())
+                    })?;
+                    Ok(())
+                }),
+                element!("td", |td| {
+                    let mut s = (*shared).borrow_mut();
+                    if s.week == 0 {
+                        return Ok(());
+                    }
+                    let rows = match td.get_attribute("rowspan") {
+                        Some(rowspan) => rowspan.parse()?,
+                        None => 1,
+                    };
+                    match s.state {
+                        State::ExpectCouple => {
+                            s.couple.clear();
+                            // When couples dance multiple dances in a show, the Couples column will
+                            // have a rowspan > 1. Keep the rowspan as the repeat count.
+                            s.couple_uses = rows;
+                            let shared = shared.clone();
+                            td.on_end_tag(move |_| {
+                                let mut s = (*shared).borrow_mut();
+                                s.state = if s.score_uses == 0 {
+                                    State::ExpectScore
+                                } else if s.dance_uses == 0 {
+                                    State::ExpectDance
+                                } else {
+                                    State::ExpectEnd
+                                };
+                                Ok(())
+                            })?;
                         }
-                        State::ExpectEnd(rows, couple) => {
-                            // ignore columns until we see the end of row
-                            State::ExpectEnd(rows, couple)
+                        State::ExpectScore => {
+                            s.score.clear();
+                            s.score_uses = rows;
+                            if rows > 1 {
+                                // In Series 10, Week 10 couples danced two styles in one dance. For
+                                // this week, the scores have rowspan > 1.
+                                let len = s.note.len();
+                                s.note.replace_range(..len, "combined dance");
+                            } else {
+                                s.note.clear();
+                            }
+                            let shared = shared.clone();
+                            td.on_end_tag(move |_| {
+                                let mut s = (*shared).borrow_mut();
+                                s.state = if s.dance_uses == 0 {
+                                    State::ExpectDance
+                                } else {
+                                    State::ExpectEnd
+                                };
+                                Ok(())
+                            })?;
+                        }
+                        State::ExpectDance => {
+                            s.dance.clear();
+                            s.dance_uses = rows;
+                            let shared = shared.clone();
+                            td.on_end_tag(move |_| {
+                                let mut s = (*shared).borrow_mut();
+                                s.state = State::ExpectEnd;
+                                Ok(())
+                            })?;
                         }
                         _ => {
-                            panic!("Unexpected state");
+                            // ignore other td elements
                         }
-                    };
-                    shared.set(s);
+                    }
                     Ok(())
                 }),
                 text!("td *", |t| {
@@ -185,29 +240,19 @@ pub(crate) fn extract_rows(series: u16, page: String) -> Result<Vec<Row>, Rewrit
                         // ignore text in sub-elements of td
                         return Ok(());
                     }
-                    let s = shared.take();
+                    let mut s = (*shared).borrow_mut();
                     match s.state {
-                        State::ExpectCouple(..)
-                        | State::ExpectScore(..)
-                        | State::ExpectDance(..) => {
-                            buffer.borrow_mut().push_str(t.as_str());
+                        State::ExpectCouple => {
+                            s.couple.push_str(t.as_str());
+                        }
+                        State::ExpectScore => {
+                            s.score.push_str(t.as_str());
+                        }
+                        State::ExpectDance => {
+                            s.dance.push_str(t.as_str());
                         }
                         _ => {}
                     }
-                    shared.set(s);
-                    Ok(())
-                }),
-                // New table row - reset search for td elements
-                element!("tr", |_el| {
-                    let mut s = shared.take();
-                    if let State::ExpectEnd(rows, couple) = s.state {
-                        s.state = if rows == 0 {
-                            State::ExpectRow
-                        } else {
-                            State::SkipCouple(rows, couple)
-                        }
-                    }
-                    shared.set(s);
                     Ok(())
                 }),
             ],
@@ -218,7 +263,8 @@ pub(crate) fn extract_rows(series: u16, page: String) -> Result<Vec<Row>, Rewrit
 
     rewriter.write(page.as_ref())?;
     rewriter.end()?;
-    Ok(output.into_inner())
+    let result = output.replace(Vec::new());
+    Ok(result)
 }
 
 #[cfg(test)]
