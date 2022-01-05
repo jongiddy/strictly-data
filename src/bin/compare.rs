a0c040875d39d96b7d366f1b773b7f2b5f561233
Jonathan Giddy <jongiddy@gmail.com>
2022-01-05 07:26:35 +0000

    Move compare binary to top level

--- src/bin/compare.rs
+++ src/bin/compare.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct Row {
@@ -22,9 +23,13 @@ pub(crate) struct UltimateRow {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Parsing strictly-data csv");
+    const TOP_DIR: Option<&str> = option_env!("CARGO_MANIFEST_DIR");
+    let top_dir = Path::new(TOP_DIR.unwrap_or("."));
+
+    let csv_file = top_dir.join("output.csv");
+    println!("Parsing {}", csv_file.display());
     let mut my_scores = HashMap::<String, Vec<u8>>::new();
-    let f = File::open("../../output.csv")?;
+    let f = File::open(csv_file)?;
     let reader = BufReader::new(f);
     let mut rdr = csv::Reader::from_reader(reader);
     for result in rdr.deserialize() {
@@ -34,9 +39,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         entry.push(record.total_score);
     }
 
-    println!("Parsing Ultimate Strictly csv");
+    let csv_file = top_dir.join("ultimate/SCD_Series18.csv");
+    println!("Parsing {}", csv_file.display());
     let mut us_scores = HashMap::<String, Vec<u8>>::new();
-    let f = File::open("../../ultimate/SCD_Series18.csv")?;
+    let f = File::open(csv_file)?;
     let reader = BufReader::new(f);
     let mut rdr = csv::Reader::from_reader(reader);
     for result in rdr.deserialize() {
